7fd13c46a50d64b8c0b16f3c469bba0966107f4a
Léo Delplanque <delplanqueleomail@gmail.com>
2020-03-10 09:14:16 +0100

    Affichage de tous les quizz avec le fichier examples.js et possibilité de voir toutes les questions en cliquant sur le nom du quizz

--- back/server.js
+++ back/server.js
@@ -1,4 +1,17 @@
 
 
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    res.set('Content-Type', 'text/html');
+    res.send();
+});
+
+app.listen(port, () => {
+    console.log('Server app listening on port ' + port);
+});
+
 
 console.log("This is the back !! Let's have fun with express.js");
--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,118 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
-function App() {
+import './examples.js';
+import {quizzes} from "./examples";
+
+import {HTTP_SERVER_PORT_PICTURES} from "./constants";
+
+
+function App()  {
+    const [Questions, setQuestions] = useState(quizzes);
+    function Home() {
+
+
+        function QuizzThumbnail(props) {
+
+            return (
+
+                <a href={'quizz/'+ props.id} ><div className="quizz">
+
+                    <img src={HTTP_SERVER_PORT_PICTURES + props.icon} alt="icon"/>
+
+
+
+                    <span>{props.name}</span>
+
+
+                </div>
+                    </a>
+
+            );
+        }
+        return (
+            <div>
+
+                <p>{Questions.map(p =>
+
+                        <QuizzThumbnail
+
+                            name = {p.name}
+                            icon = {p.icon}
+                            id = {p.id}
+
+                        />)}
+                </p>
+            </div>
+
+        );
+
+
+    }
+
+    function About() {
+        return (
+
+            <div>
+                about
+            </div>
+        );
+    }
+    
+    function Quizz(props) {
+        let id = props.match.params.id;
+
+        let jsxQuestions = Questions
+                                .filter(p => p.id == id ? true : false)
+                                .map(p => p.questions.map(p =>
+                                        <Question
+
+                                        question = {p.question}
+
+
+                                        />));
+        return (
+            <div>
+                quizz n° {id}
+
+                <p>
+                    {jsxQuestions}
+                </p>
+            </div>
+        );
+
+        function Question(props){
+            return (
+                <div>
+                    <p> Question : {props.question}</p>
+
+                </div>
+            );
+        }
+    }
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route exact={true} path="/" component={Home} />
+            <Route exact={true} path="/about" component={About} />
+            <Route exact={true} path="/quizz/:id" component={Quizz} />
+
+
+
+            <Route path="*" component={() => <p>Page Not Found</p>} />
+
+          </Switch>
+        </div>
+      </BrowserRouter>
   );
 }
+
 
 export default App;
